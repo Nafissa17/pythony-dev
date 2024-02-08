19d7fe5c91f730c64a715c2e11064ed979394596
Nafissa <loadjianaffissatou@gmail.com>
2024-02-08 11:16:22 +0100

    Fina

---
+++ hello.py
@@ -0,0 +1,46 @@
+
+print("Hello world !")
+
+print("Slt c'est Fina ")
+
+
+
+
+# Ceci est un commentaire
+
+
+age = 19
+prenom = "Nafissa"
+temperature = 3.4
+aimePizza = True
+
+
+ecole = "EPSI"
+#va chercher la valeur de la variable
+print(ecole)
+#va chercher la valeur demandée directement
+print("ecole")
+
+print("EPSI")
+
+
+#si on deux types différents on peut convertir l'un des deux en le précisant comme le b dans cet exmple
+a = 4
+b = "5"
+bInt = int(b)
+c = a + bInt
+print(c)
+
+name = "SAMB "
+prenom = "Nafissa"
+complet = name + prenom
+print(complet)
+
+
+# str c'est pour faire passer int en string
+
+age = input("Quel age avez-vous ?")
+
+age = int(age) + 1
+
+print("L annee prochaine vous aurez " + str(age) + " ans")
